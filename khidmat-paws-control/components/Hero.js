6c8c74f80f15897a2cbc93a70295647aceecae15
Nehal Naeem Haji <nh07884@st.habib.edu.pk>
2025-08-30 12:02:18 +0500

    Update ERD

--- khidmat-paws-control/components/Hero.js
+++ khidmat-paws-control/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className='container mx-auto px-4 sm:px-6 md:px-8 flex flex-col md:flex-row justify-between items-center mt-8 mb-4'>
       <div className='flex flex-col items-start gap-6 w-3/5'>
         <h1 className='text-darkbrown font-lato text-3xl md:text-4xl lg:text-5xl font-bold italic'>PAWS: Giving Every Paw a Second Chance</h1>
-				<h3 className='text-gray-400 leading-relaxed text-lg md:text-xl w-4/5'>At PAWS, we’re more than a shelter – we’re a lifeline for abandoned animals seeking love, care, and forever homes. Join us in transforming their stories of neglect into tales of hope and happiness. Together, we can make every paw count. 🐾❤️</h3>
+				<h3 className='text-gray-400 leading-relaxed text-lg md:text-xl w-4/5'>At PAWS, we're more than a shelter - we're a lifeline for abandoned animals seeking love, care, and forever homes. Join us in transforming their stories of neglect into tales of hope and happiness. Together, we can make every paw count. 🐾❤️</h3>
 				<button className='block px-4 md:px-8 py-1.5 md:py-3 mb-2 leading-loose text-md text-center text-white font-bold bg-lightbrown hover:bg-darkbrown  rounded-full'>
           <Link href='/pet-catalog'>
             Explore Pets
